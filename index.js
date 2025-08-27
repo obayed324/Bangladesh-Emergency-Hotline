6f3614c979e68122685ad1a83e908cb6061c1ac2
Mohammad Obayed Sarker <127292015+obayed324@users.noreply.github.com>
2025-08-27 21:53:18 +0600

    All heart count is done

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ function redHeart(id) {
     document.getElementById("red-heart").innerText = ++redHeart;
 
 }
+
 document.getElementById("national-emergency-heart").addEventListener("click",function(){
     redHeart("national-emergency-heart")
-})
+});
+
+document.getElementById("police-heart").addEventListener("click",function(){
+    redHeart("police-heart")
+});
+document.getElementById("fire-service-hear").addEventListener("click",function(){
+    redHeart("fire-service-hear")
+});
+document.getElementById("ambulance-heart").addEventListener("click",function(){
+    redHeart("ambulance-heart")
+});
+document.getElementById("woman-help-heart").addEventListener("click",function(){
+    redHeart("woman-help-heart")
+});
+document.getElementById("Corruption-heart").addEventListener("click",function(){
+    redHeart("Corruption-heart")
+});
+document.getElementById("electricity-heart").addEventListener("click",function(){
+    redHeart("Corruption-heart")
+});
+document.getElementById("brac-heart").addEventListener("click",function(){
+    redHeart("brac-heart")
+});
+document.getElementById("railway-heart").addEventListener("click",function(){
+    redHeart("railway-heart")
+});
